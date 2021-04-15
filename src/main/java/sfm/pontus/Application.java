fb04c4200d49e10e4a670576188e2e8641e5ae3e
danko.tibi <danko.tibi@citromail.hu>
2021-04-15 14:11:47 +0200

    Integráció
    Elkezdtem az integrációt a Netbeans projektből IntelliJ-be. Illetve régi .fxml fájlok voltak bent azokat újra szerkeztettem.

--- src/main/java/sfm/pontus/Application.java
+++ src/main/java/sfm/pontus/Application.java
@@ -1,9 +1,17 @@
+/*
 package sfm.pontus;
 
 import java.sql.SQLException;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.h2.tools.Server;
 
 public class Application {
@@ -32,4 +40,120 @@ public class Application {
     private static void startDatabase() throws SQLException {
         new Server().runTool("-tcp", "-web", "-ifNotExists");
     }
+}
+
+*/
+package sfm.pontus;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+//Tibi importjai
+import javafx.scene.image.Image;
+import javafx.stage.StageStyle;
+import java.sql.*;
+
+
+public class Application extends javafx.application.Application {
+
+        private static Scene scene;
+    private Stage primaryLogin;
+
+    @Override
+        public void start(Stage Login) throws IOException {
+            scene = new Scene(loadFXML("Login"), 640, 480);
+            Login.setScene(scene);
+            Login.show();
+        }
+
+        static void setRoot(String fxml) throws IOException {
+            scene.setRoot(loadFXML(fxml));
+        }
+
+        private static Parent loadFXML(String fxml) throws IOException {
+            FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource(fxml + ".fxml"));
+            return fxmlLoader.load();
+        }
+
+        public static void main(String[] args) {
+            launch(args);
+        }
+
+
+    @Override
+    public void start(Stage primaryLogin) throws Exception {
+
+        //loading the source from the login fxml file
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/Login.fxml"));
+        primaryLogin.setTitle("Pontus műszaki üzlet ");
+        //Setting the stage style undecorated to use custom buttons
+        primaryLogin.initStyle(StageStyle.UNDECORATED);
+        primaryLogin.setScene(new Scene(root));
+        primaryLogin.getIcons().add(new Image("Store/Képek/bejelentkező.jpg"));
+        primaryLogin.setResizable(false);
+        primaryLogin.show();
+
+    }
+
+
+//
+
+    private  static final String CONN = "jdbc:h2:mem:my_database";
+      private static final String USERNAME="sa";
+      private static final String PASSWORD="sa";
+
+//
+
+    public static Connection connectDB() {
+        try {
+            Connection con = DriverManager.getConnection(CONN, USERNAME, PASSWORD);
+            return con;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public static void App(String[] args){launch(args);}
+
+
+
+    public static ResultSet executeQueryforRS(String statement){
+        Connection connection;
+        Statement st;
+        ResultSet rs = null;
+        try {
+            connection = Application.connectDB();
+            st = connection.createStatement();
+            rs = st.executeQuery(statement);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rs;
+
+    }
+
+    public static void executeQueryforUpdate(String statement){
+        Connection connection;
+        Statement st;
+        try {
+            connection = Application.connectDB();
+            st = connection.createStatement();
+            st.executeUpdate(statement);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+
 }
