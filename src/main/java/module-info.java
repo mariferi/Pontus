3506c2ac0c2e561c127576a6245bfbcba47ca6bd
danko.tibi <danko.tibi@citromail.hu>
2021-04-16 12:31:17 +0200

    Module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,11 @@ module sfm.pontus {
     requires javafx.fxml;
     requires java.sql;
     requires java.desktop;
+    requires javafx.base;
+    requires javafx.graphics;
+
+
+
 
     opens sfm.pontus to javafx.fxml;
     exports sfm.pontus;
