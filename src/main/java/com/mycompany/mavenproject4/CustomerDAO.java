7f28ac9ae9fc4f773d076eb4baa68273d811b2ec
kalmancheyn <79642599+kalmancheyn@users.noreply.github.com>
2021-04-27 11:59:08 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/mavenproject4/CustomerDAO.java
@@ -0,0 +1,16 @@
+package com.mycompany.mavenproject4;
+
+import javax.persistence.Id;
+import java.util.List;
+
+public interface CustomerDAO extends AutoCloseable{
+
+    public void saveCustomer (Customer customer);
+    public void deleteCustomer (Customer customer);
+    public void updateCustomer (Customer customer);
+    public List<Customer> getCustomersAll();
+    public List<Customer> getCustomersbyName(String name);
+    public Customer getCustomerbyID(int id);
+
+}
+
---
+++ src/main/java/com/mycompany/mavenproject4/ProductDAO.java
@@ -0,0 +1,12 @@
+package com.mycompany.mavenproject3;
+
+import java.util.List;
+
+public interface ProductDAO extends AutoCloseable{
+    public void saveProduct (Product product);
+    public void deleteProduct (Product product);
+    public void updateProduct (Product product);
+    public List<Product> getProductsAll();
+    public List<Product> getProductsbyName(String name);
+    public Product getProductbyID(int id);
+}
