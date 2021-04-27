e53ab3089e075b9322b9d5d3fedd63b9563063ac
kalmancheyn <79642599+kalmancheyn@users.noreply.github.com>
2021-04-27 12:02:21 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/mavenproject4/Stock.java
@@ -0,0 +1,48 @@
+package com.mycompany.mavenproject3;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Stock {
+
+    private int id;
+
+    private Set<Product> products=new HashSet<>();
+
+    private  String Name;
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    @Basic
+    public String getName() {
+        return Name;
+    }
+
+    public Stock(String name) {
+        Name = name;
+    }
+
+    @Id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final Integer id) {
+        this.id = id;
+    }
+
+    @OneToMany
+    @JoinColumn(name = "Raktár_id")
+    public  Set<Product> getProducts(){
+        return products;
+    }
+
+
+    public void setProducts(Set<Product> products) {
+        this.products = products;
+    }
+}
